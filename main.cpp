2d9201ca30d09578b05b9a36988bb59be5ed2ccb
chirsz-ever <chirsz-ever@outlook.com>
2025-06-29 00:43:51 +0800

    global display

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
-int main(void)
-{
-    struct wl_display *display = wl_display_connect(NULL);
-    if (display) {
-        printf("Connected!\n");
-    } else {
-        printf("Error connecting ;(\n");
-        return 1;
-    }
+void registry_global_handler(void *data, struct wl_registry *registry, uint32_t name, const char *interface,
+                             uint32_t version) {
+    printf("interface: '%s', version: %u, name: %u\n", interface, version, name);
+}
 
-    wl_display_disconnect(display);
-    return 0;
+void registry_global_remove_handler(void *data, struct wl_registry *registry, uint32_t name) {
+    printf("removed: %u\n", name);
+}
+
+int main(void) {
+    struct wl_display *display                    = wl_display_connect(NULL);
+    struct wl_registry *registry                  = wl_display_get_registry(display);
+    struct wl_registry_listener registry_listener = {.global        = registry_global_handler,
+                                                     .global_remove = registry_global_remove_handler};
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+
+    while (1) {
+        wl_display_dispatch(display);
+    }
 }
