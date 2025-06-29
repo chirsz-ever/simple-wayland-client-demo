17cf7b6c7afc6fcc4a0f5b22e3f67867da68d9a9
chirsz-ever <chirsz-ever@outlook.com>
2025-06-30 01:51:59 +0800

    reorder headeres

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#include <cursor-shape-v1-client-protocol.h>
 #include <sys/mman.h>
 #include <sys/syscall.h>
-#include <time.h>
 #include <unistd.h>
+
 #include <wayland-client-protocol.h>
 #include <wayland-client.h>
 #include <wayland-cursor.h>
-#include <xdg-decoration-unstable-v1-client-protocol.h>
-#include <xdg-shell-client-protocol.h>
+
+#include "cursor-shape-v1-client-protocol.h"
+#include "xdg-decoration-unstable-v1-client-protocol.h"
+#include "xdg-shell-client-protocol.h"
 
 struct wl_compositor *compositor;
 struct wl_shm *shm;
